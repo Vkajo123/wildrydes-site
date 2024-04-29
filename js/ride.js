76ade2a626c897b1b80f3363891d931eef5c98ba
Vkajo123 <157926384+Vkajo123@users.noreply.github.com>
2024-04-28 22:29:11 -0500

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -152,64 +152,6 @@ let map;
 
         WildRydes.map.animate(origin, dest, callback);
     }
-(function esriMapScopeWrapper($) {
-    var wrMap = WildRydes.map;
-
-    // Put the provided animate function here
-    wrMap.animate = function animate(origin, dest, callback) {
-        let tick = 0;
-        let id = null;
-        const unicorn = WildRydes.unicorn;
-        const map = WildRydes.map.map;
-
-        let latlng = unicorn.getLatLng();
-        let latInc = (dest.latitude - latlng.lat) / 100;
-        let lngInc = (dest.longitude - latlng.lng) / 100;
-
-        // Clear any existing polyline
-        if (WildRydes.polyline) {
-            WildRydes.polyline.remove();
-        }
-
-        // Create an array to store the coordinates of the polyline
-        const coordinates = [latlng];
-
-        clearInterval(id);
-        id = setInterval(frame, 5);
-
-        function frame() {
-            if (tick === 100) {
-                clearInterval(id);
-                // Draw polyline
-                WildRydes.polyline = L.polyline(coordinates, { color: 'red' }).addTo(map);
-                callback();
-            } else {
-                tick++;
-                latlng = { lat: latlng.lat + latInc, lng: latlng.lng + lngInc };
-                unicorn.setLatLng(latlng);
-                coordinates.push(latlng); // Add new coordinate to the array
-            }
-        }
-    }
-
-    wrMap.unsetLocation = function unsetLocation() {
-        if (WildRydes.marker)
-            WildRydes.marker.remove();
-    };
-    
-(function rideScopeWrapper($) {
-    var authToken;
-    WildRydes.authToken.then(function setAuthToken(token) {
-        if (token) {
-            authToken = token;
-        } else {
-            window.location.href = '/signin.html';
-        }
-    }).catch(function handleTokenError(error) {
-        alert(error);
-        window.location.href = '/signin.html';
-    });
-
 
 }(jQuery));
 
